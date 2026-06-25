303db625577dd6466a8be2d2c990e53ab1551dd8
UFierro13212 <uriel.fierro@udla.edu.ec>
2026-06-24 22:56:32 -0500

    Gestión de Entrada y Buffer

--- Qutoaire.c
+++ Qutoaire.c
@@ -1,17 +1,33 @@
-#include <stdio.h>
-#include <string.h>
+void limpiarBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
 
-#define ZONAS 5
+void ingresarDatos(Zona zonas[]) {
+    int i;
 
-typedef struct {
-    char nombre[30];
-    float pm25;
-    float co2;
-    float temperatura;
-    float humedad;
-    float viento;
-    float prediccion;
-    char alerta[30];
-    char medida[100];
-} Zona;
+    for (i = 0; i < ZONAS; i++) {
+        printf("\n--- Zona %d ---\n", i + 1);
 
+        printf("Nombre de la zona: ");
+        fgets(zonas[i].nombre, 30, stdin);
+        zonas[i].nombre[strcspn(zonas[i].nombre, "\n")] = '\0';
+
+        printf("PM2.5: ");
+        scanf("%f", &zonas[i].pm25);
+
+        printf("CO2: ");
+        scanf("%f", &zonas[i].co2);
+
+        printf("Temperatura: ");
+        scanf("%f", &zonas[i].temperatura);
+
+        printf("Humedad: ");
+        scanf("%f", &zonas[i].humedad);
+
+        printf("Velocidad del viento: ");
+        scanf("%f", &zonas[i].viento);
+
+        limpiarBuffer();
+    }
+}
